5477448061cc4d16492353fa384627347c0f3795
qjp <qiujianp123@163.com>
2020-06-10 13:38:19 +0800

    jsx语法的使用

---
+++ src/03_jsx注意事项.js
@@ -0,0 +1,48 @@
+// 1、导入React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let a = 10;
+let b = 'str字符串';
+let boo = true;
+let title = '6666';
+const h1 = <h1>标题1</h1>;
+const arr = [
+    <h2>标题2</h2>,
+    <h3>标题3</h3>
+]
+const arrStr = ['小米','大米','红米','黑米'];
+
+// 方案1：定义一个空数组，用来存放 名称，标签
+// 注意：React 中，需要把 key 添加给 被 forEach 或 map 或 for 循环直接控制的元素
+const nameArr = [];
+arrStr.forEach( (item, index) => {
+    const temp = <h5 key={index}>{item}</h5>;
+    nameArr.push(temp);
+})
+
+// 数组 map 方法,必须写 return 方法
+const res = arrStr.map( (item) => {
+    return <h5>{item}</h5>;
+})
+
+// 3.调用 render 函数
+// 当需要在 jsx 控制的区域内，写 js 表达式，则需要把 js 代码写到 {} 中
+ReactDOM.render(<div>
+    { a + 11 }
+    <hr/>
+    { b } , { 'smdx' }
+    <hr/>
+    { boo ? '对的' : '错的' }
+    <p title={title}>这是文本 p 标签</p>
+    <hr/>
+    { h1 }
+    <hr/>
+    {/* { arr } */}
+    <hr/>
+    { nameArr }
+    <hr/>
+    { arrStr.map((item,index) => <h3 key={index}>{item}</h3> ) }
+    <p className='dddd'>123456</p>
+    <label htmlFor="ddd">haha:</label>
+    </div>, document.getElementById("app"));
